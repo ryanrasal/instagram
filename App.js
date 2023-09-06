76450e9adb33e0b97d9e92eb84f70c0eaae95d50
ryanrasal <ryan.beaujot@outlook.fr>
2023-09-06 15:42:37 +0200

    create projet and install tailwind

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
+      <View className="bg-red-500 h-60"/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
